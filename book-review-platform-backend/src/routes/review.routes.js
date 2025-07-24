1255e61cca62e01e993aa3017e22874ceb134fec
Ankit Tiwari <ankit827691@gmail.com>
2025-07-24 12:18:31 +0530

    Add review routes for managing reviews by book ID

---
+++ book-review-platform-backend/src/routes/review.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  addReview,
+  getReviewsByBookId,
+} from "../controllers/review.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = express.Router({ mergeParams: true });
+
+router.get("/", getReviewsByBookId); 
+router.post("/", verifyJWT, addReview); 
+
+export default router;

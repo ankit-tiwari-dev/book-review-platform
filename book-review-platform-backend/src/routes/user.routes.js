9d67d6fe1757cba703788917bde6fe215a7d13bd
Ankit Tiwari <ankit827691@gmail.com>
2025-07-24 12:18:37 +0530

    Refactor user routes to improve structure and readability

--- book-review-platform-backend/src/routes/user.routes.js
+++ book-review-platform-backend/src/routes/user.routes.js
@@ -1,7 +1,30 @@
 import { Router } from "express";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+} from "./../controllers/user.controller.js"
+
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
+router.post("/register", registerUser);  
+
+router.post("/login", loginUser);   
+
+router.post("/logout", logoutUser); 
+
+router.route('/refresh-token').post(refreshAccessToken)
+
+router.get("/me", verifyJWT, getCurrentUser); 
+
+router.get("/update-password", verifyJWT, changeCurrentPassword)
+
+router.route('/update-account').patch(verifyJWT, updateAccountDetails)
 
 export default router 
